66b23b8c21a71b31008702ea121df8ee2845fcc5
milexm <mielem@gmail.com>
2016-08-06 12:40:28 -0700

    Added class inheritance, simplified input processing, simplified termination.

--- aws-s3-client/src/main/java/com/acloudysky/s3/SimpleUI.java
+++ aws-s3-client/src/main/java/com/acloudysky/s3/SimpleUI.java
@@ -4,6 +4,8 @@ package com.acloudysky.s3;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+import com.acloudysky.s3.Utility;
+
 
 /*** 
  * Display a selection menu for the user. Process the
@@ -13,7 +15,7 @@ import java.io.IOException;
  * @author Michael Miele.
  *
  */
-public class SimpleUI {
+public class SimpleUI extends UserInterface {
 	
 	private StringBuilder menu;
 	private static final String newline = System.getProperty("line.separator");
@@ -21,29 +23,12 @@ public class SimpleUI {
 	private String bucketName, keyName;
 	
 	/**
-	 * Initializes the menu that allows the user to make the allowed choices.
-	 * It uses a StringBuilder to create the formatted menu.
+	 * Instantiates SimpleUI class along with its superclass.
 	 */
 	SimpleUI() {
-		menu = new StringBuilder();
-		menu.append(String.format("Select one of the following options:%n"));	
-		menu.append(String.format("%s %s Create bucket.", newline, "b1"));
-		menu.append(String.format("%s %s List buckets.", newline, "b2"));
-		menu.append(String.format("%s %s Delete bucket.", newline, "b3"));
-		
-		menu.append(String.format("%s %s Upload object.", newline, "o1"));
-		menu.append(String.format("%s %s Download object.", newline, "o2"));
-		menu.append(String.format("%s %s List objects.", newline, "o3"));
-		menu.append(String.format("%s %s Delete object.", newline, "o4"));
-		
-		menu.append(String.format("%s %s  Display menu.", newline, "m"));
-		menu.append(String.format("%s %s  Exit application.", newline, "x"));
-		menu.append(newline);		
-		
-	
+		super();
 		// Display menu.
-		System.out.println(menu.toString());
-		
+		Utility.displayMenu(Utility.getMenuEntries());
 		
 	}
 	
@@ -74,156 +59,162 @@ public class SimpleUI {
 		return selection;
 
 	}
+	
+	/**
+	 * Execute the selected operation.
+	 */
+	private void performOperation(String operation) {
+	
+		// Select operation to perform.
+		switch(operation) {
+		
+			case "cb": {
+			
+				try{
+					// Create a bucket.
+					do {
+						System.out.println(String.format("%s", "The bucket name must follow the format: chowx-i-92aea747.d.eanitea.com"));
+						bucketName = readUserInput("Bucket name: ").toLowerCase();	
+					} while(bucketName.isEmpty());
+					BucketOperations.CreateBucket(bucketName);
+				}
+				catch (Exception e){
+					System.out.println(String.format("%s", e.getMessage()));
+				}
+				break;
+			}
+			
+			case "lb": {
+				try{
+					// List the buckets contained in the account.
+					BucketOperations.ListBuckets();
+				}
+				catch (Exception e){
+					System.out.println(String.format("%s", e.getMessage()));
+				}
+				break;
+			}
+			
+			case "db": {
+				try{
+					// Delete a bucket.
+					do {
+						bucketName = readUserInput("Bucket name: ").toLowerCase();	
+					}while(bucketName.isEmpty());	
+					BucketOperations.DeleteBucket(bucketName);
+				}
+				catch (Exception e){
+					System.out.println(String.format("%s", e.getMessage()));
+				}
+				break;
+			}
+			
+			case "uo": {
+				try{
+					// Upload an object in the specified bucket.
+					do {
+						bucketName = readUserInput("Bucket name: ").toLowerCase();	
+						keyName = readUserInput("Key name: ").toLowerCase();	
+					}while(bucketName.isEmpty() || keyName.isEmpty());
+					ObjectOperations.UploadObject(bucketName, keyName);
+				}
+				catch (Exception e){
+					System.out.println(String.format("%s", e.getMessage()));
+				}
+				break;
+			}
+				
+			case "do": {
+				try{
+					// Download an object.
+					do {
+						bucketName = readUserInput("Bucket name: ").toLowerCase();	
+						keyName = readUserInput("Key name: ").toLowerCase();	
+					}while(bucketName.isEmpty() || keyName.isEmpty());
+					ObjectOperations.DownloadObject(bucketName, keyName);
+				}
+				catch (Exception e){
+					System.out.println(String.format("%s", e.getMessage()));
+				}
+				break;
+			}
+			
+			case "lo": {
+				try{
+					// List objects contained in the specified bucket.
+					do {
+						bucketName = readUserInput("Bucket name: ").toLowerCase();	
+					}while(bucketName.isEmpty());
+					ObjectOperations.ListObject(bucketName);
+				}
+				catch (Exception e){
+					System.out.println(String.format("%s", e.getMessage()));
+				}
+				break;
+			}
+			
+			case "xo": {
+				try{
+					// Delete an object.
+					do {
+						bucketName = readUserInput("Bucket name: ").toLowerCase();	
+						keyName = readUserInput("Key name: ").toLowerCase();	
+					}while(bucketName.isEmpty() || keyName.isEmpty());
+					ObjectOperations.DeleteObject(bucketName, keyName);
+				}
+				catch (Exception e){
+					System.out.println(String.format("%s", e.getMessage()));
+				}
+				break;
+			}
+				
+			
+			default: {
+				System.out.println(String.format("%s is not allowed", operation));
+				break;
+			}
+		}
+				
+
+	}
+	
+	
 	/***
 	 * Get user selection and call the related method.
 	 * Loop indefinitely until the user exits the application.
 	 */
 	public void processUserInput() {
 		
-		String in = null;
 		while (true) {
 			
 			// Get user input.
-			String selection = readUserInput(null).toLowerCase();	
+			String userSelection = readUserInput(null).toLowerCase();	
+			// Normalize user's input.
+			String normalizedUserSelection = userSelection.trim().toLowerCase();
 			
+
 			try{
 				// Exit the application.
-				if ("x".equals(selection))
+				if ("x".equals(normalizedUserSelection)){
 					break;
+				}
 				else
-					if ("m".equals(selection)) {
+					if ("m".equals(normalizedUserSelection)) {
 						// Display menu
-						System.out.println(menu.toString());
+						Utility.displayMenu(Utility.getMenuEntries());
 						continue;
 					}
-					else 
-						// Read the input string.
-						in = selection.trim();
-	
+				
 			}
 			catch (Exception e){
 				// System.out.println(e.toString());
-				System.out.println(String.format("Input %s is not allowed%n", selection));
+				System.out.println(String.format("Input %s is not allowed%n", userSelection));
 				continue;
 			}
 			
-			// Select action to perform.
-			switch(in) {
-			
-				case "b1": {
-				
-					try{
-						// Create a bucket.
-						do {
-							System.out.println(String.format("%s", "The bucket name must follow the format: chowx-i-92aea747.d.eanitea.com"));
-							bucketName = readUserInput("Bucket name: ").toLowerCase();	
-						} while(bucketName.isEmpty());
-						BucketOperations.CreateBucket(bucketName);
-					}
-					catch (Exception e){
-						System.out.println(String.format("%s", e.getMessage()));
-					}
-					break;
-				}
-				
-				case "b2": {
-					try{
-						// List the buckets contained in the account.
-						BucketOperations.ListBuckets();
-					}
-					catch (Exception e){
-						System.out.println(String.format("%s", e.getMessage()));
-					}
-					break;
-				}
-				
-				case "b3": {
-					try{
-						// Delete a bucket.
-						do {
-							bucketName = readUserInput("Bucket name: ").toLowerCase();	
-						}while(bucketName.isEmpty());	
-						BucketOperations.DeleteBucket(bucketName);
-					}
-					catch (Exception e){
-						System.out.println(String.format("%s", e.getMessage()));
-					}
-					break;
-				}
-				
-				case "o1": {
-					try{
-						// Upload an object in the specified bucket.
-						do {
-							bucketName = readUserInput("Bucket name: ").toLowerCase();	
-							keyName = readUserInput("Key name: ").toLowerCase();	
-						}while(bucketName.isEmpty() || keyName.isEmpty());
-						ObjectOperations.UploadObject(bucketName, keyName);
-					}
-					catch (Exception e){
-						System.out.println(String.format("%s", e.getMessage()));
-					}
-					break;
-				}
-					
-				case "o2": {
-					try{
-						// Download an object.
-						do {
-							bucketName = readUserInput("Bucket name: ").toLowerCase();	
-							keyName = readUserInput("Key name: ").toLowerCase();	
-						}while(bucketName.isEmpty() || keyName.isEmpty());
-						ObjectOperations.DownloadObject(bucketName, keyName);
-					}
-					catch (Exception e){
-						System.out.println(String.format("%s", e.getMessage()));
-					}
-					break;
-				}
-				
-				case "o3": {
-					try{
-						// List objects contained in the specified bucket.
-						do {
-							bucketName = readUserInput("Bucket name: ").toLowerCase();	
-						}while(bucketName.isEmpty());
-						ObjectOperations.ListObject(bucketName);
-					}
-					catch (Exception e){
-						System.out.println(String.format("%s", e.getMessage()));
-					}
-					break;
-				}
-				
-				case "o4": {
-					try{
-						// Delete an object.
-						do {
-							bucketName = readUserInput("Bucket name: ").toLowerCase();	
-							keyName = readUserInput("Key name: ").toLowerCase();	
-						}while(bucketName.isEmpty() || keyName.isEmpty());
-						ObjectOperations.DeleteObject(bucketName, keyName);
-					}
-					catch (Exception e){
-						System.out.println(String.format("%s", e.getMessage()));
-					}
-					break;
-				}
-					
-				
-				default: {
-					System.out.println(String.format("%s is not allowed", selection));
-					break;
-				}
-			}
-					
+			performOperation(normalizedUserSelection);
 		}
-		SimpleUI.Exit();
 		
 	}
 	
-	private static void Exit() {
-		System.out.println("Bye!\n");
-		return;
-	}
 }
